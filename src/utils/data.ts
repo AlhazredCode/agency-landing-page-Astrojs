ace05d82ad7fbda3877f359265eea729957aa7af
Alhazred <alhazred.art@gmail.com>
2024-10-20 20:16:51 -0400

    estilos

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,37 +1,37 @@
 const services = [
     {
         title: "Estrategia",
-        description: "Implementa y ejecuta una estrategia de difusion y ventas efectiva para tu negocio.",
+        description: "Implementa y ejecuta una estrategia de difusión y ventas efectiva para tu negocio.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
         title: "Copywriting",
-        description: "redaccion de textos pesuasivos para paginas web, landing pages, email marketing y guiones de videos.",
+        description: "Redacción de textos persuasivos para páginas web, landing pages, email marketing y guiones de videos.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
-        title: "trafico",
-        description: "Impletacion y gestion de captacion de trafico cualificado a menor coste posible.",
+        title: "Tráfico",
+        description: "Implementación y gestión de captación de tráfico cualificado al menor coste posible.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
-        title: "heramientas",
-        description: "Automatiza y escala tu negocio a traves de su propia pagina web o aplicacion movil.",
+        title: "Heramientas",
+        description: "Automatiza y escala tu negocio a través de su propia página web o aplicación móvil.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
         title: "Ventas y Soporte",
-        description: "soporte y ventas, gestion administrativa de cobros, garantias, accesos a la plataforma, etc.",
+        description: "Soporte y ventas, gestión administrativa de cobros, garantías, accesos a la plataforma, etc.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
         title: "Videos",
-        description: "Producion y edicion audiovisual que aseguran la maxima calidad de contenido para tu negocio.",
+        description: "Producción y edición audiovisual que aseguran la máxima calidad de contenido para tu negocio.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
